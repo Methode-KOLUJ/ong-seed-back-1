0320299a3dc481c2da0211a74def6f828b2efb51
Monsieur KOLUJ <misterfrpkoluj@gmail.com>
2025-03-03 23:10:06 +0100

    Update Serveur.js

--- Serveur.js
+++ Serveur.js
@@ -11,7 +11,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://ong-seed-12.vercel.app", credentials: true }));
 app.use(express.json({ limit: "10mb" }));
 app.use(bodyParser.urlencoded({ extended: true }));
 
